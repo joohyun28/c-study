19ee051f3dbba140a44f75a12605876a264d2cd3
joohyun28 <63608587+joohyun28@users.noreply.github.com>
2020-05-26 21:26:25 +0900

    Add files via upload

---
+++ c++ 3주차 과제 2.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+void  swap(int *i, int *k)
+{
+	int temp;
+	temp = *i; 
+	*i = *k;
+	*k = temp;
+}
+	
+
+
+int main()
+{
+	int i, k;
+
+	cout << "first number >> ";
+	cin >> i;
+
+
+	cout << "second number >> ";
+	cin >> k;
+
+	cout << "swap 전 >> " << i << ' ' << k << endl;
+
+	swap(&i, &k);
+
+	cout << "swap 후 >> " << i << ' ' << k	<< endl;
+
+}
