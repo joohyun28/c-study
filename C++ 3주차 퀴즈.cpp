78370e865d52b02d95e2500e6fef2413e81b22b1
joohyun28 <63608587+joohyun28@users.noreply.github.com>
2020-06-02 19:35:39 +0900

    Add files via upload

--- C++ 3주차 퀴즈.cpp
+++ C++ 3주차 퀴즈.cpp
@@ -1,42 +1,28 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int max(int a, int b) 
 {
-	if (a > b)
-	{
-		return a;
-	}
+	if (a < b)
+		return b;
 
 	else
-	{
-		return b;
-	}
+		return a;
 }
 
 int min(int a, int b) 
 {
 	if (a < b)
-	{
 		return a;
-	}
 
 	else
-	{
 		return b;
-	}
 }
 
-int main() 
-{
+int main() {
 	int a, b;
-
 	cout << "두 수를 입력해 주세요 (띄어쓰기로 구분) >> ";
-	cin >> a;
-	cin >> b;
-
-	cout << "큰 수 >> " << max(a, b);
+	cin >> a >> b;
+	cout << endl << "큰 수 >> " << max(a, b);
 	cout << endl << "작은 수 >> " << min(a, b);
-	cout << endl;
-
 }
